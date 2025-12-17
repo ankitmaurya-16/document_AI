83c46a0f5fffc154146be17302cd9fcb03cc167a
Ankit Maurya <ankitmaurya2531@gmail.com>
2025-12-17 23:51:23 +0530

    Basic Setup

---
+++ scripts/day1.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client=OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def basic_chat():
+    user_input=input("You: ")
+    
+    response=client.responses.create(
+        model="gpt-4.1-mini",
+        input=[
+            {
+                "role":"system",
+                "content":"You are a spicy person"
+            },
+            {
+              "role":"user",
+              "content":user_input  
+            }
+        ]
+        # input=user_input
+    )
+    print("LLM: ",response.output_text)
+
+def temp_test(temp):
+    prompt= "Hello, explain recursion in short"
+    response=client.responses.create(
+        model="gpt-4.1-mini",
+        temperature=temp,
+        input=prompt
+        
+    )
+    print("LLM: ",response.output_text)
+
+if __name__=="__main__":
+    for t in [0, 0.7, 1.2]:
+        temp_test(t)
---
+++ test.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client=OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+response=client.responses.create(
+    model="gpt-4.1-mini",
+    input="Say hello in an interesting way"
+)
+
+print(response.output_text)
