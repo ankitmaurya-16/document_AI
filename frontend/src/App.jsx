6807f4e37f1c76ecf1b5b513caf1f4fddea6dbb5
Ankit Maurya <ankitmaurya2531@gmail.com>
2026-01-22 01:02:28 +0530

    fixed App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { use, useLayoutEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import ChatBox from './components/chatBox'
 import Community from './pages/community'
 import Sidebar from './components/sidebar'
 import Credits from './pages/credits'
 import Login from './pages/login'
-import Loading from './pages/loading'
+import Loading from './pages/Loading'
 import { assets } from './assets/assets'
 import { useState } from 'react'
 import './assets/prism.css'
-const App = () => {
+import { useAppContext } from './context/AppContext'
+  const App = () => {
 
   const [isMenuOpen, setIsMenuOpen]=useState(false)
-
+  const {pathname} = useLocation()
+  const {user} = useAppContext()
+  if (pathname === '/loading') {
+    return <Loading/>
+  }
   return (
     <>
     {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden dark:invert' onClick={()=>setIsMenuOpen(true)}/>}
-      <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+      {user?(
+        <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
         <div className='flex h-screen w-screen'>
           <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
           <Routes>
@@ -28,6 +34,10 @@ const App = () => {
           </Routes>
         </div>
       </div>
+      ):(
+        <div><Login/></div>
+      )}
+      
     </>
   )
 }
